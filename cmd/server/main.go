67b5e032fb790ebfcc182fbfd2553315dc06e63b
0xatanda <atanda0x@gmail.com>
2026-04-15 01:26:08 +0100

    fixed: fix the port number

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/0xatanda/genderAPI/internal/handler"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/api/classify", handler.ClassifyHandler)
-	http.ListenAndServe(":8080", nil)
+
+	log.Println("Server running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
